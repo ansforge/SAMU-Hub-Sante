069ead9fe3d6565de1c4d41736875fa99b6ffe66
Romain Fouilland <romain.fd@gmail.com>
2023-02-05 19:11:44 +0100

    🐣 First Gradle steps

---
+++ src/test/java/com/hubsante/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.hubsante;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        Example.main(new String[]{});
+    }
+}
