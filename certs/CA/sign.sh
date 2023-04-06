0e14cccf96b61c7b5b206b45ac0eea8068bed6bb
Romain Fouilland <romain.fd@gmail.com>
2023-04-06 10:48:55 +0200

    🔐 Adding SAN to Hub Sante certificate to be used within docker compose network

--- certs/CA/sign.sh
+++ certs/CA/sign.sh
@@ -4,5 +4,6 @@ then
   echo "Script should be run with DOMAIN var: $ DOMAIN=... ./sign.sh"
 else
   # Ref.: https://www.baeldung.com/openssl-self-signed-cert
+  # Additional SAN can be added in hub-sante.ext
   openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in "$DOMAIN".csr -out "$DOMAIN".crt -days 365 -CAcreateserial -extfile hub-sante.ext
 fi
