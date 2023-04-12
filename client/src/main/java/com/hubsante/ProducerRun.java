ca163c4ff2c44b9dce62325428d61a4c2b605d3a
bbo <bbonche@henix.fr>
2023-04-12 15:44:57 +0200

    rm dead code

--- client/src/main/java/com/hubsante/ProducerRun.java
+++ client/src/main/java/com/hubsante/ProducerRun.java
@@ -1,6 +1,5 @@
 package com.hubsante;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.hubsante.message.*;
@@ -8,24 +7,17 @@ import com.hubsante.message.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static com.hubsante.Utils.convertMessageFromType;
 import static com.hubsante.Utils.getRouting;
 
 public class ProducerRun {
-
     private static final String HUB_HOSTNAME = "localhost";
     private static final int HUB_PORT = 5671;
     private static final String EXCHANGE_NAME = "hubsante";
     public static void main(String[] args) throws Exception {
         String routingKey = getRouting(args);
         String json = Files.readString(Path.of(args[1]));
-        // registering extra module is mandatory to handle date time
-        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        BasicMessage basicMessage = mapper.readValue(json, BasicMessage.class);
 
         TLSConf tlsConf = new TLSConf(
                 "TLSv1.2",
@@ -36,9 +28,12 @@ public class ProducerRun {
 
         Producer producer = new Producer(HUB_HOSTNAME, HUB_PORT, EXCHANGE_NAME);
         producer.connect(tlsConf);
+
+        // registering extra module is mandatory to handle date time
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        BasicMessage basicMessage = mapper.readValue(json, BasicMessage.class);
         CisuMessage cisuMessage = convertMessageFromType(mapper, basicMessage, json.getBytes(StandardCharsets.UTF_8));
 
         producer.publish(routingKey, cisuMessage);
-//        producer.publish(routingKey, basicMessage);
     }
 }
