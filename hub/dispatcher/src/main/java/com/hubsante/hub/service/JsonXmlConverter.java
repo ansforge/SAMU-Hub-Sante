4d4c5502b952ee251da67dae9968f6dc59d9f049
bbonche <benjamin.bonche.ext@esante.gouv.fr>
2023-04-25 18:17:32 +0200

    cleanup

--- hub/dispatcher/src/main/java/com/hubsante/hub/service/JsonXmlConverter.java
+++ hub/dispatcher/src/main/java/com/hubsante/hub/service/JsonXmlConverter.java
@@ -37,7 +37,8 @@ public class JsonXmlConverter {
      * have not the same case
      * When we will use OpenAPI generator the Java classes will respect the same case as the xsd spec
      */
-    private final XmlMapper xmlMapper = (XmlMapper) new XmlMapper().registerModule(new JavaTimeModule())
+    private final XmlMapper xmlMapper = (XmlMapper) new XmlMapper()
+            .registerModule(new JavaTimeModule())
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
             .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
