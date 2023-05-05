42ba1165bc4f0c92c26941adee1f913fd8bbb515
bbonche <benjamin.bonche.ext@esante.gouv.fr>
2023-05-05 18:50:45 +0200

    add javadoc

--- client/src/main/java/com/hubsante/Utils.java
+++ client/src/main/java/com/hubsante/Utils.java
@@ -16,6 +16,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
+
+    /*
+    * Dans le contexte de ce code exemple, cette méthode permet de récupérer un clientId à partir
+    * de la routing key fournie en argument de run
+     */
     static String getClientId(String[] strings) {
         String[] routing = getRouting(strings).split("[.]");
         List<String> routingKey = Arrays.stream(routing).limit(routing.length-2).collect(Collectors.toList());
@@ -27,6 +32,10 @@ public class Utils {
         return keys[keys.length -1];
     }
 
+    /*
+    * Dans le contexte de ce code exemple, cette méthode permet de récupérer la routing key
+    * passée en argument de run
+     */
     static String getRouting(String[] strings) {
         if (strings.length < 1)
             return "anonymous.info";
