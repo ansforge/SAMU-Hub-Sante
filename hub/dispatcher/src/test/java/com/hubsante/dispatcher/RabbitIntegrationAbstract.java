baedb181a1815ef0d2f2848ba68680f5bb54d660
bbo <bbonche@henix.fr>
2023-06-28 16:40:06 +0200

    add TLS tests

--- hub/dispatcher/src/test/java/com/hubsante/dispatcher/RabbitIntegrationAbstract.java
+++ hub/dispatcher/src/test/java/com/hubsante/dispatcher/RabbitIntegrationAbstract.java
@@ -2,35 +2,34 @@ package com.hubsante.dispatcher;
 
 import com.hubsante.hub.HubApplication;
 import com.hubsante.hub.service.EdxlHandler;
+import com.rabbitmq.client.DefaultSaslConfig;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
-import java.util.Objects;
+
+import static com.hubsante.dispatcher.utils.SSLTestUtils.getSSlContext;
 
 @SpringBootTest
 @ContextConfiguration(classes = HubApplication.class
         , initializers = RabbitIntegrationTest.Initializer.class
 )
-@ActiveProfiles("test")
 @Testcontainers
 @Slf4j
 public class RabbitIntegrationAbstract {
@@ -38,39 +37,28 @@ public class RabbitIntegrationAbstract {
     protected static final String HUBSANTE_EXCHANGE = "hubsante";
     protected static final String RABBITMQ_IMAGE = "rabbitmq:3.11-management-alpine";
     protected static final String ENTRY_QUEUE = "*.out.*";
-    protected static final String SENDER_MESSAGE_ROUTING_KEY = "fr.health.samuB.out.message";
-    protected static final String SENDER_ACK_QUEUE = "fr.health.samuB.in.ack";
-    protected static final String RECIPIENT_QUEUE = "fr.fire.nexsis.in.message";
+    protected static final String SAMU_B_OUTER_MESSAGE_ROUTING_KEY = "fr.health.samuB.out.message";
+    protected static final String SAMU_B_ACK_QUEUE = "fr.health.samuB.in.ack";
+    protected static final String SDIS_Z_MESSAGE_QUEUE = "fr.fire.nexsis.sdisZ.in.message";
 
     @Autowired
     protected RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    protected RabbitTemplate samuARabbitTemplate;
-
-    @Autowired
-    protected RabbitAdmin rabbitAdmin;
 
     static ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
     @Autowired
     protected EdxlHandler converter;
-
-    @DynamicPropertySource
-    static void registerPgProperties(DynamicPropertyRegistry propertiesRegistry) {
-        propertiesRegistry.add("client.preferences.file",
-                () -> Objects.requireNonNull(classLoader.getResource("config/client.preferences.csv")));
-    }
+    protected volatile boolean failed = false;
 
     @Container
     public static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer(
             DockerImageName.parse(RABBITMQ_IMAGE))
             .withPluginsEnabled("rabbitmq_management", "rabbitmq_auth_mechanism_ssl")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("config/definitions.json"),
+            .withCopyFileToContainer(mountFile("config/definitions.json"),
                     "tmp/rabbitmq/config/definitions.json")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("config/certs/server/"),
+            .withCopyFileToContainer(mountFile("config/certs/server/"),
                     "/etc/rabbitmq-tls/")
-            .withRabbitMQConfigSysctl(MountableFile.forClasspathResource("config/rabbitmq.conf"));
+            .withRabbitMQConfigSysctl(mountFile("config/rabbitmq.conf"));
 
     @BeforeAll
     public static void beforeAll() throws IOException, InterruptedException {
@@ -81,20 +69,57 @@ public class RabbitIntegrationAbstract {
     }
 
     @AfterEach
-    public void cleanUp() {
-        rabbitAdmin.purgeQueue(ENTRY_QUEUE, false);
-        rabbitAdmin.purgeQueue(SENDER_ACK_QUEUE, false);
-        rabbitAdmin.purgeQueue(RECIPIENT_QUEUE, false);
+    public void cleanUp() throws IOException, InterruptedException {
+        rabbitMQContainer.execInContainer("rabbitmqctl", "purge_queue");
+        failed = false;
+    }
+
+    protected RabbitTemplate getCustomRabbitTemplate(String p12Path, String p12Passphrase) throws Exception {
+        com.rabbitmq.client.ConnectionFactory cf = new com.rabbitmq.client.ConnectionFactory();
+        cf.setHost(rabbitMQContainer.getHost());
+        cf.setPort(rabbitMQContainer.getAmqpsPort());
+        cf.setVirtualHost("/");
+
+        SSLContext sslContext = getSSlContext(p12Path, p12Passphrase);
+        cf.useSslProtocol(sslContext);
+
+        cf.setSaslConfig(DefaultSaslConfig.EXTERNAL);
+        CachingConnectionFactory ccf = new CachingConnectionFactory(cf);
+        ccf.setPublisherConfirmType(CachingConnectionFactory.ConfirmType.CORRELATED);
+        ccf.setPublisherReturns(true);
+
+        return new RabbitTemplate(ccf);
     }
 
     public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
         @Override
         public void initialize(ConfigurableApplicationContext applicationContext) {
             val values = TestPropertyValues.of(
+                    // broker identification
                     "spring.rabbitmq.host=" + rabbitMQContainer.getHost(),
-                    "spring.rabbitmq.port=" + rabbitMQContainer.getMappedPort(5672)
+                    "spring.rabbitmq.port=" + rabbitMQContainer.getAmqpsPort(),
+
+                    // default RabbitTemplate conf (dispatcher)
+                    "spring.rabbitmq.ssl.key-store-password=dispatcher",
+                    "spring.rabbitmq.ssl.trust-store-password=trustStore",
+                    "spring.rabbitmq.ssl.key-store=" + Thread.currentThread().getContextClassLoader()
+                            .getResource("config/certs/dispatcher/dispatcher.test.p12"),
+                    "spring.rabbitmq.ssl.trust-store=" + Thread.currentThread().getContextClassLoader()
+                            .getResource("config/certs/trustStore"),
+                    "client.preferences.file=" + Thread.currentThread().getContextClassLoader()
+                            .getResource("config/client.preferences.csv"),
+
+                    // must be set to handle PublisherConfirms in other RabbitTemplates,
+                    // even if we don't use it in Dispatcher
+                    "spring.rabbitmq.publisher-confirm-type=correlated",
+                    "spring.rabbitmq.publisher-returns=true",
+                    "spring.rabbitmq.template.mandatory=true"
             );
             values.applyTo(applicationContext);
         }
+    }
+
+    private static MountableFile mountFile(String pathInClasspath) {
+        return MountableFile.forClasspathResource(pathInClasspath);
     }
 }
--- hub/dispatcher/src/test/java/com/hubsante/dispatcher/RabbitIntegrationTest.java
+++ hub/dispatcher/src/test/java/com/hubsante/dispatcher/RabbitIntegrationTest.java
@@ -1,129 +1,64 @@
 package com.hubsante.dispatcher;
 
-import com.hubsante.hub.HubApplication;
-import com.hubsante.hub.service.EdxlHandler;
 import com.hubsante.model.edxl.EdxlMessage;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.junit.jupiter.api.*;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.core.MessagePropertiesBuilder;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.RabbitMQContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-import org.testcontainers.utility.MountableFile;
 
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSocketFactory;
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-@ContextConfiguration(classes = HubApplication.class
-        , initializers = RabbitIntegrationTest.Initializer.class
-)
-@ActiveProfiles("test")
-@Testcontainers
 @Slf4j
-public class RabbitIntegrationTest {
-    private static final String HUBSANTE_EXCHANGE = "hubsante";
-    private static final String RABBITMQ_IMAGE = "rabbitmq:3.11-management-alpine";
-    private static final String SENDER_MESSAGE_ROUTING_KEY = "fr.health.samuB.out.message";
-    private static final String RECIPIENT_QUEUE = "fr.fire.nexsis.in.message";
-
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    private RabbitAdmin rabbitAdmin;
-
-    static ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
-    @Autowired
-    private EdxlHandler converter;
-
-    @DynamicPropertySource
-    static void registerPgProperties(DynamicPropertyRegistry propertiesRegistry) {
-        propertiesRegistry.add("client.preferences.file",
-                () -> Objects.requireNonNull(classLoader.getResource("config/client.preferences.csv")));
-    }
-
-    @BeforeAll
-    public static void beforeAll() throws IOException, InterruptedException {
-        rabbitMQContainer.start();
-        // only for debug : to see the management console
-        Integer port = rabbitMQContainer.getMappedPort(15672);
-        rabbitMQContainer.execInContainer("rabbitmqctl", "import_definitions", "/tmp/rabbitmq/config/definitions.json");
-        System.out.println("tutu");
-    }
-
-    @AfterEach
-    public void cleanUp() {
-//        rabbitAdmin.purgeQueue(ENTRY_QUEUE, false);
-//        rabbitAdmin.purgeQueue(SENDER_ACK_QUEUE, false);
-//        rabbitAdmin.purgeQueue(RECIPIENT_QUEUE, false);
-    }
-
-    @Container
-    public static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer(
-            DockerImageName.parse(RABBITMQ_IMAGE))
-            .withPluginsEnabled("rabbitmq_management", "rabbitmq_auth_mechanism_ssl")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("config/definitions.json"),
-                    "tmp/rabbitmq/config/definitions.json")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("config/certs/server/"),
-                    "/etc/rabbitmq-tls/")
-            .withRabbitMQConfigSysctl(MountableFile.forClasspathResource("config/rabbitmq.conf"));
+public class RabbitIntegrationTest extends RabbitIntegrationAbstract {
 
     @Test
     @DisplayName("message dispatched to exchange is received by a consumer listening to the right queue")
-    public void dispatchTest() throws IOException, InterruptedException, KeyManagementException, NoSuchAlgorithmException {
+    public void dispatchTest() throws Exception {
         File edxlCisuCreateFile = new File(classLoader.getResource("messages/samuB_to_nexsis.xml").getFile());
         String json = Files.readString(edxlCisuCreateFile.toPath());
 
         MessageProperties properties = MessagePropertiesBuilder.newInstance().setContentType("application/xml").build();
         Message published = new Message(json.getBytes(StandardCharsets.UTF_8), properties);
 
-        rabbitTemplate.sendAndReceive(HUBSANTE_EXCHANGE, SENDER_MESSAGE_ROUTING_KEY, published);
+        RabbitTemplate samuB_client = getCustomRabbitTemplate(classLoader.getResource("config/certs/samuB/samuB.p12").getPath(), "samuB");
+        samuB_client.sendAndReceive(HUBSANTE_EXCHANGE, SAMU_B_OUTER_MESSAGE_ROUTING_KEY, published);
 
         Thread.sleep(100);
-        Message received = rabbitTemplate.receive(RECIPIENT_QUEUE);
+
+        RabbitTemplate nexsis_client = getCustomRabbitTemplate(classLoader.getResource("config/certs/sdisZ/sdisZ.p12").getPath(), "sdisZ");
+        Message received = nexsis_client.receive(SDIS_Z_MESSAGE_QUEUE);
 
         EdxlMessage publishedEdxl = converter.deserializeXmlEDXL(new String(published.getBody(), StandardCharsets.UTF_8));
         EdxlMessage receivedEdxl = converter.deserializeXmlEDXL(new String(received.getBody(), StandardCharsets.UTF_8));
         Assertions.assertEquals(publishedEdxl, receivedEdxl);
     }
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Test
+    @DisplayName("samuB can't publish without topic permissions")
+    public void unauthorizedConsumerTest() throws Exception {
+        String p12Path = classLoader.getResource("config/certs/samuB/samuB.p12").getPath();
+        RabbitTemplate samuB_client = getCustomRabbitTemplate(p12Path, "samuB");
 
-        @Override
-        public void initialize(ConfigurableApplicationContext applicationContext) {
-            val values = TestPropertyValues.of(
-                    "spring.rabbitmq.host=" + rabbitMQContainer.getHost(),
-                    "spring.rabbitmq.port=" + rabbitMQContainer.getMappedPort(5672)
-            );
-            values.applyTo(applicationContext);
-        }
+        samuB_client.setConfirmCallback((correlationData, ack, cause) -> {
+            if (!ack) {
+                failed = true;
+            }
+        });
+
+        File edxlCisuCreateFile = new File(classLoader.getResource("messages/samuB_to_nexsis.xml").getFile());
+        String xml = Files.readString(edxlCisuCreateFile.toPath());
+
+        MessageProperties properties = MessagePropertiesBuilder.newInstance().setContentType("application/xml").build();
+        Message published = new Message(xml.getBytes(StandardCharsets.UTF_8), properties);
+        samuB_client.sendAndReceive(
+                HUBSANTE_EXCHANGE, "fr.health.samuA.out.message", published);
+
+        assertTrue(failed);
     }
 }
