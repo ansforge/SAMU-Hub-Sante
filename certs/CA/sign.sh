5cb67008e22cd1481cbe46f442dee3067be828f0
Romain Fouilland <romain.fd@gmail.com>
2023-07-18 17:35:10 +0200

    Updating Hub certificate to cover message.hub.esante.gouv.fr

--- certs/CA/sign.sh
+++ certs/CA/sign.sh
@@ -16,7 +16,7 @@ else
     openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in "$DOMAIN".csr -out "$DOMAIN".crt -days 365 -CAcreateserial -extfile hub.ext
     echo "> Copy hub.crt and hub.key into hub/rabbitmq/certs"
     echo "> Delete current TLS secret: kubectl delete secret tls-secret"
-    echo "> Recreate new secret (from hub/infra): kubectl create secret tls tls-secret --cert=../rabbitmq/certs/hub.crt --key=../rabbitmq/certs/hub.key"
+    echo "> Recreate new secret (from hub/infra): kubectl create secret tls tls-secret --cert=../../hub/rabbitmq/certs/hub.crt --key=../../hub/rabbitmq/certs/hub.key"
     echo "> Update Pods to use new secret: kubectl delete pods -l app.kubernetes.io/component=rabbitmq"
     echo "> Add certificate to Client truststore (from repository root): keytool -import -alias RabbitMQHubSante -file hub/rabbitmq/certs/hub.crt -keystore certs/trustStore"
     echo "> Copy trustStore to be used in Dispatcher (from repository root): cp certs/trustStore dispatcher/src/main/jib/certs/"
