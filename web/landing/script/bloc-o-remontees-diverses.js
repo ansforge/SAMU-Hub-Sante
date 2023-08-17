22de5d903cca578d007fef5a50662fb9be91143c
Romain Fouilland <romain.fd@gmail.com>
2023-08-17 18:35:57 +0200

    Making StyleGuide Remontées Diverses work

--- web/landing/script/bloc-o-remontees-diverses.js
+++ web/landing/script/bloc-o-remontees-diverses.js
@@ -1,1 +1,4 @@
-$(document).ready((function(){fn_slider(".o-paragraph--mixed-push__teasers",".o-paragraph--mixed-push__slider","top",2,3,3)}));
+$(document).ready((function () {
+    fn_slider(".o-paragraph--mixed-push__teasers", ".o-paragraph--mixed-push__slider", "top", 2, 3, 3)
+}
+));
