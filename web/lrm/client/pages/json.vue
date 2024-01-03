ac58ddfd33f4e2b0b380632070183ec54b50dcbd
Romain Fouilland <romain.fd@gmail.com>
2024-01-03 18:43:59 +0100

    Improving JSON Builder style

--- web/lrm/client/pages/json.vue
+++ web/lrm/client/pages/json.vue
@@ -263,29 +263,59 @@ export default {
   }
 }
 </script>
-<style>
-.message {
-  transition: all 0.5s;
-}
 
-.message-enter, .message-leave-to
-  /* .message-leave-active for <2.1.8 */
-{
-  opacity: 0;
-  transform: scale(0.7) translateY(-500px);
-}
+<style lang="scss">
+// Ref.: https://github.com/chenfengjw163/vue-json-viewer/tree/master#theming
+// values are default one from jv-light template
+.json-theme {
+  background: rgba(0, 0, 0, 0);
+  white-space: nowrap;
+  color: #525252;
+  font-size: 14px;
+  font-family: Consolas, Menlo, Courier, monospace;
 
-.message-enter-to {
-  opacity: 1;
-  transform: scale(1);
-}
-
-.message-leave-active {
-  /*position: absolute;*/
-}
-
-.message-move {
-  opacity: 1;
-  transition: all 0.5s;
+  .jv-ellipsis {
+    color: #999;
+    background-color: #eee;
+    display: inline-block;
+    line-height: 0.9;
+    font-size: 0.9em;
+    padding: 0px 4px 2px 4px;
+    border-radius: 3px;
+    vertical-align: 2px;
+    cursor: pointer;
+    user-select: none;
+  }
+  .jv-button { color: #49b3ff }
+  .jv-key { color: #111111 }
+  .jv-item {
+    &.jv-array { color: #111111 }
+    &.jv-boolean { color: #fc1e70 }
+    &.jv-function { color: #067bca }
+    &.jv-number { color: #fc1e70 }
+    &.jv-number-float { color: #fc1e70 }
+    &.jv-number-integer { color: #fc1e70 }
+    &.jv-object { color: #111111 }
+    &.jv-undefined { color: #e08331 }
+    &.jv-string {
+      color: #42b983;
+      word-break: break-word;
+      white-space: normal;
+    }
+  }
+  .jv-code {
+    padding-bottom: 12px !important;
+    .jv-toggle {
+      &:before {
+        padding: 0px 2px;
+        border-radius: 2px;
+      }
+      &:hover {
+        &:before {
+          background: #eee;
+        }
+      }
+    }
+  }
 }
 </style>
