b7195faf12211d02ea7f8e1389245fd52f6c7004
bbo <bbonche@henix.fr>
2024-06-03 11:51:53 +0200

    add RS-EDA descriptors
    (cherry picked from commit a81d509a6a08b2e9da6fcb7083ed6dd38e2f8fc0)

--- web/lrm/client/store/index.js
+++ web/lrm/client/store/index.js
@@ -68,39 +68,39 @@ export const state = () => ({
       victim: 'Homme, adulte, 43 ans',
       medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
     },
-    {
-      file: 'EMSI/EMSI-RDC-message.json',
-      icon: 'mdi-bike-fast',
-      name: 'Alexandre ARMAURY (RDC)',
-      caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
-      context: 'Collision de 2 vélos',
-      environment: 'Voie cyclable à Lyon, gêne de la circulation',
-      victims: '2 victimes, 1 nécessitant assistance SAMU',
-      victim: 'Homme, adulte, 43 ans',
-      medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
-    },
-    {
-      file: 'EMSI/EMSI-OPG-message.json',
-      icon: 'mdi-bike-fast',
-      name: 'Alexandre ARMAURY (OPG)',
-      caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
-      context: 'Collision de 2 vélos',
-      environment: 'Voie cyclable à Lyon, gêne de la circulation',
-      victims: '2 victimes, 1 nécessitant assistance SAMU',
-      victim: 'Homme, adulte, 43 ans',
-      medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
-    },
-    {
-      file: 'EMSI/EMSI-Complet-DC-RDC-OPG-message.json',
-      icon: 'mdi-bike-fast',
-      name: 'Alexandre ARMAURY (EMSI)',
-      caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
-      context: 'Collision de 2 vélos',
-      environment: 'Voie cyclable à Lyon, gêne de la circulation',
-      victims: '2 victimes, 1 nécessitant assistance SAMU',
-      victim: 'Homme, adulte, 43 ans',
-      medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
-    }]
+      {
+        file: 'EMSI/EMSI-RDC-message.json',
+        icon: 'mdi-bike-fast',
+        name: 'Alexandre ARMAURY (RDC)',
+        caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
+        context: 'Collision de 2 vélos',
+        environment: 'Voie cyclable à Lyon, gêne de la circulation',
+        victims: '2 victimes, 1 nécessitant assistance SAMU',
+        victim: 'Homme, adulte, 43 ans',
+        medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
+      },
+      {
+        file: 'EMSI/EMSI-OPG-message.json',
+        icon: 'mdi-bike-fast',
+        name: 'Alexandre ARMAURY (OPG)',
+        caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
+        context: 'Collision de 2 vélos',
+        environment: 'Voie cyclable à Lyon, gêne de la circulation',
+        victims: '2 victimes, 1 nécessitant assistance SAMU',
+        victim: 'Homme, adulte, 43 ans',
+        medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
+      },
+      {
+        file: 'EMSI/EMSI-Complet-DC-RDC-OPG-message.json',
+        icon: 'mdi-bike-fast',
+        name: 'Alexandre ARMAURY (EMSI)',
+        caller: 'Albane Armaury, témoin accident impliquant son mari, Alexandre Armaury',
+        context: 'Collision de 2 vélos',
+        environment: 'Voie cyclable à Lyon, gêne de la circulation',
+        victims: '2 victimes, 1 nécessitant assistance SAMU',
+        victim: 'Homme, adulte, 43 ans',
+        medicalSituation: 'Céphalées, migraines, traumatismes sérieux, plaies intermédiaires'
+      }]
   }, {
     label: 'RS-EDA',
     schemaName: 'RS-EDA.schema.json',
@@ -115,6 +115,26 @@ export const state = () => ({
       victims: '1 victime, nécessitant assistance SAMU',
       victim: 'Homme, adulte, 65 ans',
       medicalSituation: 'Plaie traumatique profonde, perte de conscience, hémorragie importante'
+    }, {
+      file: 'RS-EDA-usecase-PartageDossier-2',
+      icon: 'mdi-image-broken-variant',
+      name: 'Lola HALIMI',
+      caller: 'Amina BERTRAND, baby-sitter de la patient, Lola HALIMI',
+      context: 'Fracture de la cheville',
+      environment: 'Cage d\'escalier d\'un immeuble résidentiel',
+      victims: '1 patient',
+      victim: 'Fille, enfant, 10 ans',
+      medicalSituation: 'Entorse de la cheville, avec possible fracture. Cheville gonflée et douloureuse. Marche impossible.'
+    }, {
+      file: 'RS-EDA-usecase-PartageDossier-3',
+      icon: 'mdi-head-heart-outline',
+      name: 'Monsieur X',
+      caller: 'Lucas Bernardi, gérant de gite touristique',
+      context: 'Suspicion d\'AVC ',
+      environment: 'Lieu accueillant du public : Le gîte du Moulin',
+      victims: '1 patient',
+      victim: 'Adulte, environ 80 ans',
+      medicalSituation: 'Confusion, étourdissements, s’exprime difficilement. Client du gite voyageant seul.'
     }, {
       file: 'RS-EDA/RS-EDA-usecase-AppelLimitrophe-2.json',
       icon: 'mdi-heart-plus-outline',
@@ -160,62 +180,62 @@ export const state = () => ({
 })
 
 export const getters = {
-  isAuthenticated (state) {
+  isAuthenticated(state) {
     return !!state.auth.user.clientId
   },
 
-  user (state) {
+  user(state) {
     return state.auth.user
   },
 
-  isAdvanced (state) {
+  isAdvanced(state) {
     return state.auth.user.advanced
   },
 
-  showSentMessages (state) {
+  showSentMessages(state) {
     return state.auth.user.showSentMessages
   },
 
-  autoAck (state) {
+  autoAck(state) {
     return state.auth.user.autoAck
   },
 
-  messages (state) {
+  messages(state) {
     return state.messages
   },
 
-  messageJustSent (state) {
+  messageJustSent(state) {
     return state.messageJustSent
   },
 
-  messageTypes (state) {
+  messageTypes(state) {
     return state.messageTypes
   }
 }
 
 export const actions = {
-  logInUser ({ state, commit }, userData) {
+  logInUser({state, commit}, userData) {
     // use state.auth.user to get default values
-    commit(SET_CURRENT_USER, { ...state.auth.user, ...userData })
+    commit(SET_CURRENT_USER, {...state.auth.user, ...userData})
     return userData
   },
 
-  toggleAdvanced ({ commit, getters }) {
+  toggleAdvanced({commit, getters}) {
     commit(TOGGLE_ADVANCED)
     return getters.isAdvanced
   },
 
-  setShowSentMessages ({ commit }, showSentMessages) {
+  setShowSentMessages({commit}, showSentMessages) {
     commit(SET_SHOW_SENT_MESSAGES, showSentMessages)
     return showSentMessages
   },
 
-  setAutoAck ({ commit }, autoAck) {
+  setAutoAck({commit}, autoAck) {
     commit(SET_AUTO_ACK, autoAck)
     return autoAck
   },
 
-  addMessage ({ commit }, message) {
+  addMessage({commit}, message) {
     commit(ADD_MESSAGE, message)
     // If sending message worked well
     if (message.direction === '→') { // isOUt() check
@@ -226,32 +246,32 @@ export const actions = {
     }
   },
 
-  resetMessages ({ commit }) {
+  resetMessages({commit}) {
     commit(RESET_MESSAGES)
   },
 
-  loadSchemas ({ state, commit }, source) {
+  loadSchemas({state, commit}, source) {
     // ToDo: load schemas from github branch directly so it is up to date?
     source = source || 'schemas/json-schema/'
-    Promise.all(state.messageTypes.map(async ({ schemaName }, index) => {
+    Promise.all(state.messageTypes.map(async ({schemaName}, index) => {
       console.log('Loading schema from: ' + source + schemaName)
       const response = await fetch(source + schemaName)
       const schema = await response.json()
-      return ({ index, schema })
+      return ({index, schema})
     })).then((schemas) => {
-      schemas.forEach(({ index, schema }) => {
-        commit(SET_MESSAGE_TYPE_SCHEMA, { index, schema })
+      schemas.forEach(({index, schema}) => {
+        commit(SET_MESSAGE_TYPE_SCHEMA, {index, schema})
       })
     })
   }
 }
 
 export const mutations = {
-  [SET_CURRENT_USER] (state, user) {
+  [SET_CURRENT_USER](state, user) {
     state.auth.user = user
   },
 
-  [TOGGLE_ADVANCED] (state) {
+  [TOGGLE_ADVANCED](state) {
     // Not picked up by Vue reactivity (getter not updated): state.auth.user.advanced = !state.auth.user.advanced
     state.auth.user = {
       ...state.auth.user,
@@ -259,33 +279,33 @@ export const mutations = {
     }
   },
 
-  [SET_SHOW_SENT_MESSAGES] (state, showSentMessages) {
+  [SET_SHOW_SENT_MESSAGES](state, showSentMessages) {
     state.auth.user = {
       ...state.auth.user,
       showSentMessages
     }
   },
 
-  [SET_AUTO_ACK] (state, autoAck) {
+  [SET_AUTO_ACK](state, autoAck) {
     state.auth.user = {
       ...state.auth.user,
       autoAck
     }
   },
 
-  [ADD_MESSAGE] (state, message) {
+  [ADD_MESSAGE](state, message) {
     state.messages.unshift(message)
   },
 
-  [SET_MESSAGE_JUST_SENT] (state, messageJustSent) {
+  [SET_MESSAGE_JUST_SENT](state, messageJustSent) {
     state.messageJustSent = messageJustSent
   },
 
-  [RESET_MESSAGES] (state) {
+  [RESET_MESSAGES](state) {
     state.messages = []
   },
 
-  [SET_MESSAGE_TYPE_SCHEMA] (state, { index, schema }) {
+  [SET_MESSAGE_TYPE_SCHEMA](state, {index, schema}) {
     Vue.set(state.messageTypes, index, {
       ...this.state.messageTypes[index],
       schema
