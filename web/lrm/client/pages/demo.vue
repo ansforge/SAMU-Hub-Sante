63848098eb5ce047d48164d59265c094a1ce1a40
ssviridov.henix <ssviridov@henix.fr>
2024-06-14 16:58:46 +0200

    feat/lrm: add logic necessary to dynamically load messageTypes from the targeted models repository

--- web/lrm/client/pages/demo.vue
+++ web/lrm/client/pages/demo.vue
@@ -193,7 +193,6 @@ export default {
   mounted () {
     // To automatically generate the UI and input fields based on the JSON Schema
     this.$store.dispatch('loadSchemas', REPOSITORY_URL + this.$config.modelBranch + '/src/main/resources/json-schema/')
-    this.$store.dispatch('loadMessageTypes', REPOSITORY_URL + this.$config.modelBranch + '/src/main/resources/lrm/messageTypes.json')
   },
   methods: {
     typeMessages (type) {
