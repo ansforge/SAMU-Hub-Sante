f266d372a9755b3a258eed36b2006505263d2b5a
ssviridov <sav.sviridov@henix.fr>
2024-10-04 17:15:29 +0200

    feat/lrm: add 'review'-based coloring to test case steps and fix a breaking bug related to websocket name

--- web/lrm/client/pages/test/case.vue
+++ web/lrm/client/pages/test/case.vue
@@ -100,55 +100,50 @@
               </p>
               <v-list>
                 <v-list-item v-for="(requiredValue, name, index) in getAwaitedValues(testCase?.steps[currentStep-1])" :key="'requiredValue' + index">
-                  <span style="display: flex; flex-direction: row; align-items: center;">
-                    <pre class="values"><b>{{ name }}:</b><br> {{ requiredValue.value }}</pre>
-                  </span>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
+                  <div class="d-flex">
+                    <span>
+                      <v-icon v-if="testCase?.steps[currentStep-1]?.requiredValues[index]?.valid === 'valid'" style="flex:0" color="success">
+                        mdi-check
+                      </v-icon>
 
-            <v-card-title>
-              Valeurs reçues dans le message
-            </v-card-title>
-            <v-card-text v-if="testCase?.steps[currentStep-1]?.type === 'receive'">
-              <v-list>
-                <!-- Generate a list of paths:values from required values and add three buttons for each entry, letting user indicate whether the value they received is correct, 'somewhat' correct or incorrect-->
-                <v-list-item v-for="(requiredValue, index) in testCase?.steps[currentStep-1].requiredValues" :key="'requiredValue' + index" class="received-values-list">
-                  <span class="d-flex flex-row align-center">
-                    <span class="confirmation-buttons d-flex flex-row">
-                      <!-- Grey out buttons that do not correspond to the validation state if the value has already been validated -->
-                      <v-btn density="compact" icon="mdi-check" :color="(requiredValue.valid === 'valid' || requiredValue.valid === undefined) ? 'success' : 'grey' " @click="requiredValue.valid = 'valid'" />
-                      <v-btn density="compact" icon="mdi-tilde" :color="(requiredValue.valid === 'approximate' || requiredValue.valid === undefined) ? 'warning' : 'grey' " @click="requiredValue.valid = 'approximate'" />
-                      <v-btn density="compact" icon="mdi-close" :color="(requiredValue.valid === 'invalid' || requiredValue.valid === undefined) ? 'error' : 'grey' " @click="requiredValue.valid = 'invalid'" />
+                      <v-icon v-else style="flex:0" color="error">
+                        mdi-close
+                      </v-icon>
                     </span>
                     <span>
-                      <pre class="values" :style="{color: requiredValue.valid === 'valid' ? 'green' : requiredValue.valid === 'approximate' ? 'orange' : requiredValue.valid === 'invalid' ? 'red' : 'black'}"><b>{{ requiredValue.path.join('.') }}:</b> <br>{{ requiredValue.value }}</pre>
+                      <pre><b>{{ name }}:</b></pre>
+                      <pre>{{ requiredValue.value }}</pre>
+                      <pre v-if="!validatedValue?.valid" class="wrong-received">(Reçu: {{ validatedValue?.receivedValue || 'null' }}) </pre>
                     </span>
+                  </div>
+                </v-list-item>
+              </v-list>
+            </v-card-text>
 
-                  </span>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
-          </span>
-          <!-- Currently selected message's valid and invalid required values -->
-          <span v-if="selectedMessage&&!isOut(selectedMessage.direction)&&selectedMessage?.validatedValues?.length>0">
-            <v-card-title>
-              {{ 'Valeurs reçues dans le message séléctionné' }}
-            </v-card-title>
-            <v-card-text>
-              <v-list>
-                <v-list-item v-for="(validatedValue, index) in selectedMessage?.validatedValues" :key="'validatedValue' + index" class="d-flex flex-row flex-wrap">
-                  <span class="d-flex flex-row flex-wrap">
-                    <v-icon v-if="validatedValue.valid" style="flex:0" color="success">
-                      mdi-check
-                    </v-icon> <v-icon v-else style="flex:0" color="error">
-                      mdi-close
-                    </v-icon>
-                    <pre class="values"><b>{{ validatedValue?.value?.path?.join('.') }}:</b> <br>{{ validatedValue?.value?.value }}<br><span v-if="!validatedValue?.valid" class="wrong-received">(Reçu: {{ validatedValue?.receivedValue || 'null' }}) </span></pre>
-                  </span>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
+            <div v-if="testCase?.steps[currentStep-1]?.type === 'receive'">
+              <v-card-title>
+                Valeurs reçues dans le message
+              </v-card-title>
+              <v-card-text>
+                <v-list>
+                  <!-- Generate a list of paths:values from required values and add three buttons for each entry, letting user indicate whether the value they received is correct, 'somewhat' correct or incorrect-->
+                  <v-list-item v-for="(requiredValue, index) in testCase?.steps[currentStep-1].requiredValues" :key="'requiredValue' + index" class="received-values-list">
+                    <span class="d-flex flex-row align-center">
+                      <span class="confirmation-buttons d-flex flex-row">
+                        <!-- Grey out buttons that do not correspond to the validation state if the value has already been validated -->
+                        <v-btn density="compact" icon="mdi-check" :color="(requiredValue.valid === 'valid' || requiredValue.valid === undefined) ? 'success' : 'grey' " @click="requiredValue.valid = 'valid'" />
+                        <v-btn density="compact" icon="mdi-tilde" :color="(requiredValue.valid === 'approximate' || requiredValue.valid === undefined) ? 'warning' : 'grey' " @click="requiredValue.valid = 'approximate'" />
+                        <v-btn density="compact" icon="mdi-close" :color="(requiredValue.valid === 'invalid' || requiredValue.valid === undefined) ? 'error' : 'grey' " @click="requiredValue.valid = 'invalid'" />
+                      </span>
+                      <span>
+                        <pre class="values" :style="{color: requiredValue.valid === 'valid' ? 'green' : requiredValue.valid === 'approximate' ? 'orange' : requiredValue.valid === 'invalid' ? 'red' : 'black'}"><b>{{ requiredValue.path.join('.') }}:</b> <br>{{ requiredValue.value }}</pre>
+                      </span>
+
+                    </span>
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+            </div>
           </span>
         </template>
         <template v-else>
@@ -429,15 +424,28 @@ function checkMessageContainsAllRequiredValues (message, requiredValues) {
 }
 
 function getStepColor (index) {
-  if (index === currentlySelectedStep.value - 1) {
-    return 'primary'
-  } else if (index === currentStep.value - 1) {
-    return '#CFE2F6'
-  } else if (index < currentStep.value - 1) {
-    return 'green'
+  // For reception steps, color is determined by the average color of the received values
+  if (testCase.value.steps[index].type === 'receive') {
+    const counts = getCounts(testCase.value.steps[index])
+    return getAverageColor(counts.unreviewed, counts.valid, counts.approximate, counts.invalid, counts.total)
   } else {
-    return 'grey'
+    // For send steps, color is determined by the validation state of the step
+    return testCase.value.steps[index]?.validatedReceivedValues ? 'success' : 'grey'
   }
+}
+
+function getAverageColor (unset, success, warning, error, total) {
+  // grey: #9e9e9e, success: #4caf50, warning: #fb8c00, error: #b00020, total: #000000
+  const unsetPercent = (unset / total) * 100
+  const successPercent = (success / total) * 100
+  const warningPercent = (warning / total) * 100
+  const errorPercent = (error / total) * 100
+
+  const red = Math.round((unsetPercent * 158 + successPercent * 76 + warningPercent * 251 + errorPercent * 176) / 100)
+  const green = Math.round((unsetPercent * 158 + successPercent * 175 + warningPercent * 140 + errorPercent * 0) / 100)
+  const blue = Math.round((unsetPercent * 158 + successPercent * 80 + warningPercent * 0 + errorPercent * 32) / 100)
+
+  return `rgb(${red}, ${green}, ${blue})`
 }
 
 function setSelectedMessage (message) {
@@ -493,6 +501,7 @@ watch(selectedTypeCaseMessages, (newMessages) => {
 
 <script>
 export default {
+  name: 'Testcase',
   mixins: [mixinMessage],
   beforeRouteEnter (to, from) {
     if (!useMainStore().isAuthenticated) {
