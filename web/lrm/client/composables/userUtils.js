0a52243d5c314f7ad8dd26964b1aff51f3a12b5f
ssviridov <sav.sviridov@henix.fr>
2024-10-18 10:06:03 +0200

    hotfix/lrm: fix composables store usage not being implemented correctly

--- web/lrm/client/composables/userUtils.js
+++ web/lrm/client/composables/userUtils.js
@@ -1,8 +1,10 @@
 import { useMainStore } from '~/store'
 
-const store = useMainStore()
-
-export function clientInfos (clientId = store.user.clientId) {
+export function clientInfos (clientId = null) {
+  if (!clientId) {
+    const store = useMainStore()
+    clientId = store.user.clientId
+  }
   return {
     name: clientId.split('.').splice(2).join('.'), // Remove the first two parts of the clientId (ex: fr.health)
     icon: clientId.split('.')[1] === 'health' ? 'mdi-heart-pulse' : 'mdi-fire'
