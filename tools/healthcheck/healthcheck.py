d6abfeacd86fdba4b38c9cf19a4cee6f0f72d007
bnx <benjamin.bonche@gmail.com>
2024-11-19 16:32:21 +0100

    variabilize dispatcher instances

--- tools/healthcheck/healthcheck.py
+++ tools/healthcheck/healthcheck.py
@@ -8,11 +8,9 @@ RABBITMQ_URL = os.getenv("RABBITMQ_URL", "http://rabbitmq-server.default.svc:156
 RABBITMQ_MONITORING_USERNAME = os.getenv("RABBITMQ_MONITORING_USERNAME", "guest")
 RABBITMQ_MONITORING_PASSWORD = os.getenv("RABBITMQ_MONITORING_PASSWORD", "guest")
 RABBITMQ_CA_CERT_PATH = '/etc/ssl/certs/hubsante-rabbitmq-ca.crt'
-# PROMETHEUS_URL = os.getenv("PROMETHEUS_URL", "http://prometheus-server.default.svc:9090")
-SPRING_APPS = [
-    {"name": "dispatcher-15-15-v1-5-service", "url": "http://spring-app-1/actuator/health"},
-    {"name": "dispatcher-15-nexsis-v1-9-service", "url": "http://spring-app-2/actuator/health"},
-]
+
+DISPATCHER_INSTANCES_ENV_VAR = os.getenv("DISPATCHER_INSTANCES", "")
+DISPATCHER_INSTANCES = DISPATCHER_INSTANCES_ENV_VAR.split(",") if DISPATCHER_INSTANCES_ENV_VAR else []
 
 def rabbitmq_healthcheck():
     try:
@@ -47,9 +45,9 @@ def health():
         global_status = "DOWN"
 
     # Fetch health from Spring apps
-    for app in SPRING_APPS:
-        spring_health = dispatcher_healthcheck(app)
-        components[app] = spring_health
+    for dispatcher_instance in DISPATCHER_INSTANCES:
+        spring_health = dispatcher_healthcheck(dispatcher_instance)
+        components[dispatcher_instance] = spring_health
         if spring_health["status"] == "DOWN":
             global_status = "DOWN"
 
