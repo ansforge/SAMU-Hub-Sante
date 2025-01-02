6c9fba14703db143dc7ca52886a81c879c625ada
Clément Di Domenico <clement.didom@gmail.com>
2025-01-02 17:47:44 +0100

    refactor/lrm/server: extract modeles method in dedicated service

--- web/lrm/server/router/modelesRouter.js
+++ web/lrm/server/router/modelesRouter.js
@@ -1,74 +1,13 @@
 const express = require('express');
-const octokit = require('octokit');
 const config = require('../config');
-
-const client = new octokit.Octokit({ auth: process.env.GITHUB_TOKEN });
-
-const OWNER = 'ansforge';
-const REPO = 'SAMU-Hub-Modeles';
-const EXAMPLE_FILES_PATH = 'src/main/resources/sample/examples';
-
-const getModelesBranchesHandler = async (_, res) => {
-  const response = await client.rest.repos.listBranches({
-    owner: OWNER,
-    repo: REPO,
-  });
-  res.status(200).json(response.data.map(({ name }) => name));
-};
-
-const generateCommitMessage = (fileName) => `Update of the json example ${fileName}`;
-
-const createNewBranch = async ({
-  baseBranch,
-  newBranch,
-}) => {
-  const baseBranchCommit = await client.rest.repos.getCommit({
-    owner: OWNER,
-    repo: REPO,
-    ref: baseBranch,
-  });
-
-  const baseCommitSha = baseBranchCommit.data.sha;
-
-  await client.rest.git.createRef({
-    owner: OWNER,
-    repo: REPO,
-    ref: `refs/heads/${newBranch}`,
-    sha: baseCommitSha,
-  });
-};
-
-const commitModelesChangesToExistingBranch = async ({
-  branch,
-  fileName,
-  content,
-}) => {
-  const filePath = `${EXAMPLE_FILES_PATH}/${fileName}`;
-
-  const fileShaResponse = await client.rest.repos.getContent({
-    owner: OWNER,
-    repo: REPO,
-    path: filePath,
-    ref: branch,
-  });
-  const fileSha = fileShaResponse.data.sha;
-
-  const encodedContent = Buffer.from(content).toString('base64');
-
-  const response = await client.rest.repos.createOrUpdateFileContents({
-    owner: OWNER,
-    repo: REPO,
-    path: filePath,
-    message: generateCommitMessage(fileName),
-    content: encodedContent,
-    sha: fileSha,
-    branch,
-  });
-
-  return response.data;
-};
+const { getModelesBranchNames, createNewBranch, commitModelesChangesToExistingBranch } = require('../service/modeles');
 
 const VALIDATION_ERROR_MESSAGE = 'Missing mandatory attribute in payload';
+
+const getModelesBranchesHandler = async (_, res) => {
+  const branchNames = await getModelesBranchNames();
+  res.status(200).json(branchNames);
+};
 
 const validatePayload = (body) => {
   if (!body.fileName) {
---
+++ web/lrm/server/service/modeles.js
@@ -0,0 +1,73 @@
+const octokit = require('octokit');
+
+const client = new octokit.Octokit({ auth: process.env.GITHUB_TOKEN });
+
+const OWNER = 'ansforge';
+const REPO = 'SAMU-Hub-Modeles';
+const EXAMPLE_FILES_PATH = 'src/main/resources/sample/examples';
+
+const generateCommitMessage = (fileName) => `Update of the json example ${fileName}`;
+
+const createNewBranch = async ({
+  baseBranch,
+  newBranch,
+}) => {
+  const baseBranchCommit = await client.rest.repos.getCommit({
+    owner: OWNER,
+    repo: REPO,
+    ref: baseBranch,
+  });
+
+  const baseCommitSha = baseBranchCommit.data.sha;
+
+  await client.rest.git.createRef({
+    owner: OWNER,
+    repo: REPO,
+    ref: `refs/heads/${newBranch}`,
+    sha: baseCommitSha,
+  });
+};
+
+const commitModelesChangesToExistingBranch = async ({
+  branch,
+  fileName,
+  content,
+}) => {
+  const filePath = `${EXAMPLE_FILES_PATH}/${fileName}`;
+
+  const fileShaResponse = await client.rest.repos.getContent({
+    owner: OWNER,
+    repo: REPO,
+    path: filePath,
+    ref: branch,
+  });
+  const fileSha = fileShaResponse.data.sha;
+
+  const encodedContent = Buffer.from(content).toString('base64');
+
+  const response = await client.rest.repos.createOrUpdateFileContents({
+    owner: OWNER,
+    repo: REPO,
+    path: filePath,
+    message: generateCommitMessage(fileName),
+    content: encodedContent,
+    sha: fileSha,
+    branch,
+  });
+
+  return response.data;
+};
+
+const getModelesBranchNames = async () => {
+  const response = await client.rest.repos.listBranches({
+    owner: OWNER,
+    repo: REPO,
+  });
+  return response.data.map(({ name }) => name);
+};
+
+module.exports = {
+  createNewBranch,
+  commitModelesChangesToExistingBranch,
+  getModelesBranchNames,
+};
