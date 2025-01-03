a8b61f935e0ddff6e262201e37432ad68a68c638
Clément Di Domenico <clement.didom@gmail.com>
2025-01-03 10:20:01 +0100

    chore/lrm/server: move files to src & migrate index to ts

---
+++ web/lrm/server/src/index.ts
@@ -0,0 +1,21 @@
+import config from './config';
+import logger from './logger';
+import ExpressServer from './expressServer';
+
+class App {
+  private expressServer: ExpressServer | undefined;
+  public launchServer = async () => {
+    try {
+      this.expressServer = new ExpressServer(config.URL_PORT);
+      this.expressServer.launch();
+      logger.info('Express server running');
+    } catch (error) {
+      console.error(error);
+      // @ts-ignore
+      logger.error(`Express Server failure: ${error.message}`);
+      await this.expressServer?.close();
+    }
+  };
+}
+
+(new App()).launchServer().catch((e) => logger.error(`Error during server launch: ${e}`));
