e9830a857c8a988ca27e7ad333ff969ce166f0b9
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-01-21 17:09:20 +0100

    feat(java tutorial): tuto send message

--- clients/src/main/java/com/hubsante/Constants.java
+++ clients/src/main/java/com/hubsante/Constants.java
@@ -7,5 +7,6 @@ public final class Constants {
     public static final String JSON_CONTENT_TYPE = "application/json";
     public static final String XML_CONTENT_TYPE = "application/xml";
     public static final String JSON_FILE_EXTENSION = "json";
+    public static final String XML_FILE_EXTENSION = "xml";
     public static final Integer NETWORK_RECOVERY_INTERVAL = 1000;
 }
