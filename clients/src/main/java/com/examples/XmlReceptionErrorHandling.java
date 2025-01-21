5b747b4ccf0cd58b18eaf1e91f60ea3ff13ba851
muffoltz <99671294+muffoltz@users.noreply.github.com>
2025-01-21 17:08:23 +0100

    feat(java tutorial): tuto basic  message reception and error handling

---
+++ clients/src/main/java/com/examples/XmlReceptionErrorHandling.java
@@ -0,0 +1,69 @@
+package com.examples;
+
+import com.hubsante.Consumer;
+import com.hubsante.TLSConf;
+import com.hubsante.model.edxl.EdxlMessage;
+import com.rabbitmq.client.Delivery;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static com.hubsante.Constants.TLS_PROTOCOL_VERSION;
+import static com.hubsante.Utils.*;
+
+public class XmlReceptionErrorHandling {
+    private static final Logger logger = LoggerFactory.getLogger(XmlReceptionErrorHandling.class);
+
+    public static void main(String[] args) throws Exception {
+        Dotenv dotenv = Dotenv.load();
+
+        TLSConf tlsConf = new TLSConf(
+                TLS_PROTOCOL_VERSION,
+                dotenv.get("KEY_PASSPHRASE"),
+                dotenv.get("CERTIFICATE_PATH"),
+                dotenv.get("TRUST_STORE_PASSWORD"),
+                dotenv.get("TRUST_STORE_PATH"));
+
+        String queueName = getRouting(args);
+        String clientId = getClientId(args);
+
+        Consumer consumer = new Consumer(dotenv.get("HUB_HOSTNAME"), Integer.parseInt(dotenv.get("HUB_PORT")), dotenv.get("VHOST"),
+                dotenv.get("EXCHANGE_NAME"),
+                queueName, clientId) {
+            @Override
+            protected void deliverCallback(String consumerTag, Delivery delivery) throws IOException {
+                String routingKey = delivery.getEnvelope().getRoutingKey();
+
+                String message = convertBytesToString(delivery.getBody());
+                EdxlMessage edxlMessage;
+                String stringMessage;
+
+                try {
+                    edxlMessage = edxlHandler.deserializeXmlEDXL(message);
+                    stringMessage = edxlHandler.serializeXmlEDXL(edxlMessage);
+                } catch (IOException error) {
+                    logger.error("[x] Error when receiving message: '"+  error.getMessage());
+
+                    // Send back technical non ACK as delivery responsibility is removed from the Hub
+                    consumeChannel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+
+                    return;
+                }
+                logger.info("[x] Received from '" + routingKey + "':'" + stringMessage + "'");
+
+                consumeChannel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+
+                // Apply business rules
+                // ...
+                // If an error occurs, send a message to the "info" queue
+
+            }
+        };
+
+        consumer.connect(tlsConf);
+        logger.info(" [*] Waiting for messages on " + queueName + ". To exit press CTRL+C");
+    }
+
+}
