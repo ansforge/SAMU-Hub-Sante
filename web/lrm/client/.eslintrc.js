2a698d56c768bcef497b6fc44cfea3bd2d1792e9
Issam KADAR <issam.kadar@gmail.com>
2025-01-23 09:14:57 +0100

    feature/lrm : update jslinter

--- web/lrm/client/.eslintrc.js
+++ web/lrm/client/.eslintrc.js
@@ -2,16 +2,31 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es2021: true
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
     'plugin:nuxt/recommended',
-    'plugin:vue/base',
-    'plugin:vuetify/base'
+    'plugin:prettier/recommended'
   ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
   plugins: [
+    '@typescript-eslint',
+    'vue',
+    'prettier'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-debugger': 'warn'
+  }
 }
