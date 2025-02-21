cea0e85af2a7ea17ba14afb3ec34af1fe7f0c5c7
Romain Fouilland <32517197+romainfd@users.noreply.github.com>
2025-02-21 18:26:09 +0100

    Update web/lrm/client/middleware/auth.global.js

--- web/lrm/client/middleware/auth.global.js
+++ web/lrm/client/middleware/auth.global.js
@@ -3,7 +3,7 @@ import { defineNuxtRouteMiddleware, navigateTo } from 'nuxt/app';
 import { useAuthStore } from '@/store/auth'; // Adjust the path as necessary
 
 export default defineNuxtRouteMiddleware((to, _from) => {
-  const authStore = useAuthStore(); // Use Pinia store (assuming you are using Pinia)
+  const authStore = useAuthStore();
 
   if (['/', '/json'].includes(to.path)) {
     return; // Allow access to these pages
