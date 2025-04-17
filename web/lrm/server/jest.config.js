b3e74d50f486ee2e394066d8ac09e73c9c9159d7
Clément Di Domenico <clement.didom@gmail.com>
2025-04-17 15:55:53 +0200

    chore(lrm/backend): add text-summary coverage reporter (to match client)

--- web/lrm/server/jest.config.js
+++ web/lrm/server/jest.config.js
@@ -39,12 +39,13 @@ const config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    "text",
+    "text-summary",
+    "lcov",
+    "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
