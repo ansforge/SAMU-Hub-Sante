fa1ab0fa43b9fae3723e0664e141c06c0c49b454
Clément Di Domenico <clement.didom@gmail.com>
2025-04-18 16:51:22 +0200

    fix(lrm/client): ignore coverage & ignore require needed in cypress config

--- web/lrm/client/cypress.config.js
+++ web/lrm/client/cypress.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     experimentalStudio: true,
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('@cypress/code-coverage/task')(_on, _config);
       return _config;
     },
