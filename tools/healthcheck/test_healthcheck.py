965dbf16dd276db49327f72050b66c98743af3e2
Clément Di Domenico <clement.didom@gmail.com>
2025-04-22 14:55:05 +0200

    refactor(tools/healthcheck): parameterized status tests

--- tools/healthcheck/test_healthcheck.py
+++ tools/healthcheck/test_healthcheck.py
@@ -1,18 +1,21 @@
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 import json
-from healthcheck import app, remove_error_keys
+from healthcheck import app
 
 class HealthCheckTestCase(unittest.TestCase):
-
+    @parameterized.expand([
+        ([{ "status": "ok" }, { "status": "UP", "components": {} }], "UP", "UP", "UP"),
+        ([{ "status": "down" }, { "status": "UP", "components": {} }], "DOWN", "DOWN", "UP"),
+        ([{ "status": "ok" }, { "status": "DOWN", "components": {} }], "DOWN", "UP", "DOWN"),
+        ([{ "status": "down" }, { "status": "DOWN", "components": {} }], "DOWN", "DOWN", "DOWN")
+    ])
     @patch("requests.get")
-    def test_health_check_rabbitmq_up_and_dispatcher_up(self, mock_get):
+    def test_health_check(self, side_effect, global_status, rabbitmq_status, dispatcher_status, mock_get):
         # Simulate RabbitMQ & dispatcher returning healthy statuses
         mock_get.return_value.status_code = 200
-        mock_get.return_value.json.side_effect = [
-            { "status": "ok" },
-            { "status": "UP", "components": {} }
-        ]
+        mock_get.return_value.json.side_effect = side_effect
         
         # Call the route and test the response
         with patch("healthcheck.DISPATCHER_INSTANCES", ["dispatcher1"]):
@@ -20,66 +23,9 @@ class HealthCheckTestCase(unittest.TestCase):
                 response = client.get("/health")
                 self.assertEqual(response.status_code, 200)
                 data = json.loads(response.data)
-                self.assertEqual(data["status"], "UP")
-                self.assertEqual(data["components"]["rabbitmq_server"]["status"], "UP")
-                self.assertEqual(data["components"]["dispatcher1"]["status"], "UP")
-    
-    @patch("requests.get")
-    def test_health_check_rabbitmq_down_and_dispatcher_up(self, mock_get):
-        # Simulate RabbitMQ & dispatcher returning healthy statuses
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.side_effect = [
-            { "status": "down" },
-            { "status": "UP", "components": {} }
-        ]
-        
-        # Call the route and test the response
-        with patch("healthcheck.DISPATCHER_INSTANCES", ["dispatcher1"]):
-            with app.test_client() as client:
-                response = client.get("/health")
-                self.assertEqual(response.status_code, 200)
-                data = json.loads(response.data)
-                self.assertEqual(data["status"], "DOWN")
-                self.assertEqual(data["components"]["rabbitmq_server"]["status"], "DOWN")
-                self.assertEqual(data["components"]["dispatcher1"]["status"], "UP")
-    
-    @patch("requests.get")
-    def test_health_check_rabbitmq_up_and_dispatcher_down(self, mock_get):
-        # Simulate RabbitMQ & dispatcher returning healthy statuses
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.side_effect = [
-            { "status": "ok" },
-            { "status": "DOWN", "components": {} }
-        ]
-        
-        # Call the route and test the response
-        with patch("healthcheck.DISPATCHER_INSTANCES", ["dispatcher1"]):
-            with app.test_client() as client:
-                response = client.get("/health")
-                self.assertEqual(response.status_code, 200)
-                data = json.loads(response.data)
-                self.assertEqual(data["status"], "DOWN")
-                self.assertEqual(data["components"]["rabbitmq_server"]["status"], "UP")
-                self.assertEqual(data["components"]["dispatcher1"]["status"], "DOWN")
-
-    @patch("requests.get")
-    def test_health_check_rabbitmq_down_and_dispatcher_down(self, mock_get):
-        # Simulate RabbitMQ & dispatcher returning healthy statuses
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.json.side_effect = [
-            { "status": "down" },
-            { "status": "DOWN", "components": {} }
-        ]
-        
-        # Call the route and test the response
-        with patch("healthcheck.DISPATCHER_INSTANCES", ["dispatcher1"]):
-            with app.test_client() as client:
-                response = client.get("/health")
-                self.assertEqual(response.status_code, 200)
-                data = json.loads(response.data)
-                self.assertEqual(data["status"], "DOWN")
-                self.assertEqual(data["components"]["rabbitmq_server"]["status"], "DOWN")
-                self.assertEqual(data["components"]["dispatcher1"]["status"], "DOWN")
+                self.assertEqual(data["status"], global_status)
+                self.assertEqual(data["components"]["rabbitmq_server"]["status"], rabbitmq_status)
+                self.assertEqual(data["components"]["dispatcher1"]["status"], dispatcher_status)
     
 if __name__ == '__main__':
     unittest.main()
