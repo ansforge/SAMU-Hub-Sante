55be67673a6037a66cb3d591b262679f0c26e618
Clément Di Domenico <clement.didom@gmail.com>
2025-05-07 12:39:08 +0200

    fix(dispatcher): conversion util logs & variable in tests

--- hub/dispatcher/src/test/java/com/hubsante/hub/service/DispatcherTest.java
+++ hub/dispatcher/src/test/java/com/hubsante/hub/service/DispatcherTest.java
@@ -114,7 +114,7 @@ public class DispatcherTest {
         propertiesRegistry.add("client.preferences.file",
                 () -> Objects.requireNonNull(classLoader.getResource("config/client.preferences.csv")));
         propertiesRegistry.add("hubsante.default.message.ttl", () -> 5);
-        propertiesRegistry.add("dispatcher.vhost", () -> "15-15_v3.0");
+        propertiesRegistry.add("spring.rabbitmq.virtual-host", () -> "15-15_v3.0");
     }
 
     @PostConstruct
