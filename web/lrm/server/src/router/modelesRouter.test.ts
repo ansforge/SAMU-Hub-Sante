0da2a30bc211a163ea877013cbd2ef32920c3521
Issam Kadar <issam@kadar.pro>
2025-06-17 18:17:31 +0200

    feature/lrm : migrate lib jest to vitest to accept esm modules

--- web/lrm/server/src/router/modelesRouter.test.ts
+++ web/lrm/server/src/router/modelesRouter.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { validatePayload, CommitModelesChangesBody } from './modelesRouter';
 
 const generateValidInput = (): CommitModelesChangesBody => ({
