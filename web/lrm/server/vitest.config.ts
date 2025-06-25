5d19432d4f1fb5b70a60ac1cd08fa9be66223615
Issam KADAR <issam.kadar@gmail.com>
2025-06-25 16:10:26 +0200

    feature/lrm/server : migrate jest coverage setting to vitest

--- web/lrm/server/vitest.config.ts
+++ web/lrm/server/vitest.config.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reportsDirectory: 'coverage',
+      reporter: ['json', 'text', 'text-summary', 'lcov', 'clover'],
+      exclude: ['node_modules/', 'coverage/*'],
+      include: ['src/**/*.ts'],
+    },
   },
 });
