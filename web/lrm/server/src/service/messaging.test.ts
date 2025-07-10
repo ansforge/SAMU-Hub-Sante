b6101e63dfa9fbd97a7faad9d5bb23914881e060
Clément Di Domenico <clement.didom@gmail.com>
2025-07-10 15:06:26 +0200

    test(lrm/server): use expect.assertions

--- web/lrm/server/src/service/messaging.test.ts
+++ web/lrm/server/src/service/messaging.test.ts
@@ -34,6 +34,9 @@ vi.mock('../rabbit/utils', () => {
 
 const originalEnv = process.env;
 
+const reconnectSpy = vi.spyOn(MessagingService.prototype, 'reconnect');
+const handleChannelErrorSpy = vi.spyOn(MessagingService.prototype, 'handleChannelError');
+
 beforeEach(() => {
   vi.resetModules();
   process.env = {
@@ -54,50 +57,51 @@ afterEach(async () => {
 
 describe('Messaging service', () => {
   it('crash the app after trying to reconnect 3 times', async () => {
-    const reconnectSpy = vi.spyOn(MessagingService.prototype, 'reconnect');
-    const handleChannelErrorSpy = vi.spyOn(MessagingService.prototype, 'handleChannelError');
-
     const config = new Config();
     const connector = new RabbitMQConnector(config);
 
+    const service = new MessagingService('15-15_v1.5', config, connector, new WebSocketServer({ noServer: true }));
+
+    service.connectToVhost();
+    for (let i = 0; i < 3; i++) {
+      mockChannel.emit('error', new Error('Mock error during connection to queue'));
+    }
+
+    expect(handleChannelErrorSpy).toHaveBeenCalledTimes(3);
+    expect(reconnectSpy).toHaveBeenCalledTimes(3);
+
     try {
-      const service = new MessagingService('15-15_v1.5', config, connector, new WebSocketServer({ noServer: true }));
-
-      service.connectToVhost();
-      for (let i = 0; i < 4; i++) {
-        mockChannel.emit('error', new Error('Mock error during connection to queue'));
-      }
-
-      expect(handleChannelErrorSpy).toHaveBeenCalledTimes(4);
-      expect(reconnectSpy).toHaveBeenCalledTimes(3);
+      mockChannel.emit('error', new Error('Mock error during connection to queue'));
     } catch (err) {
       expect(err).toEqual(new Error('Mock error during connection to queue'));
     }
+
+    // Check that all the expect statements have been checked (wether valid or failing)
+    expect.assertions(3);
   });
 
   it('crash the app after receiving a missing queue error', async () => {
-    const reconnectSpy = vi.spyOn(MessagingService.prototype, 'reconnect');
-    const handleChannelErrorSpy = vi.spyOn(MessagingService.prototype, 'handleChannelError');
-
     const config = new Config();
     const connector = new RabbitMQConnector(config);
 
-    try {
-      const service = new MessagingService('15-15_v1.5', config, connector, new WebSocketServer({ noServer: true }));
+    const service = new MessagingService('15-15_v1.5', config, connector, new WebSocketServer({ noServer: true }));
 
-      service.connectToVhost();
+    service.connectToVhost();
+
+    try {
       mockChannel.emit('error', {
         code: 404,
         message: 'NOT_FOUND - no queue fr.health.test.samuV1',
       });
-
+    } catch (err) {
       expect(handleChannelErrorSpy).toHaveBeenCalledTimes(1);
       expect(reconnectSpy).not.toHaveBeenCalled();
-    } catch (err) {
       expect(err).toEqual({
         code: 404,
         message: 'NOT_FOUND - no queue fr.health.test.samuV1',
       });
     }
+    // Check that all the expect statements have been checked (wether valid or failing)
+    expect.assertions(3);
   });
 });
