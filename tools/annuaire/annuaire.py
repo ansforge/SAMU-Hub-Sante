ca47a5f95a342de50862410e4f5422eb9f56e096
Titouan Corne <titouan.corne@esante.gouv.fr>
2025-07-15 11:25:08 +0200

    Return all data from configMap. Selection of columns handled by configMap

--- tools/annuaire/annuaire.py
+++ tools/annuaire/annuaire.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 CSV_DIR = "/config"
 
-@app.get("/annuaire")
+@app.get("/api/annuaire")
 def get_json():
     filename = "rabbitmq.clients-configuration.csv"
     path = os.path.join(CSV_DIR, filename)
@@ -17,15 +17,4 @@ def get_json():
             data = list(reader)
     except Exception as e:
         abort(500, description=f"Erreur lors de la lecture du CSV: {e}")
-    data = clean_data(data)
     return jsonify(data)
-
-def clean_data(data: list[dict]) -> list[dict]:
-    """On enlève les lignes et les colonnes du csv qui ne nous intéressent pas"""
-    columns_to_remove = ['CommonName', 'additionalPermissions', 'lrm_test']
-    data_updated = []
-    for row in data:
-        if row.get('CommonName'):
-            cleaned_row = {key: value for key, value in row.items() if key not in columns_to_remove}
-            data_updated.append(cleaned_row)
-    return data_updated
