5b10d91c50b81872fea9fb83dadab700b9b6ba9f
Titouan Corne <titouan.corne@esante.gouv.fr>
2025-09-12 13:47:47 +0200

    feat(tools/annuaire): add health endpoint

--- tools/annuaire/annuaire.py
+++ tools/annuaire/annuaire.py
@@ -43,6 +43,13 @@ app = None
 if __name__ == "__main__":
     app = create_app()
 
-    @app.get(API_ENDPOINT)
-    def get_json():
-        return jsonify(app.config[CSV_DATA_KEY])
+@app.get("/annuaire/api")
+def get_json():
+    return jsonify(app.config[CSV_DATA_KEY])
+
+@app.get('annuaire/health')
+def health_check():
+    return jsonify({
+        "status": "UP",
+        "service": "SAMU Hub Annuaire"
+    }), 200
