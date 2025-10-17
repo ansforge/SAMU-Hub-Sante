3fa8f5e6427b4a98bf8098e09db1afb6503c2708
Clément Di Domenico <clement.didom@gmail.com>
2025-10-17 18:10:15 +0200

    chore(lrm/server): simplify logger config & add global error logging

--- web/lrm/server/src/logger.ts
+++ web/lrm/server/src/logger.ts
@@ -1,18 +1,24 @@
 import { transports, createLogger, format } from 'winston';
 
+const buildUnexpectedErrorsFileTransport = () =>
+  new transports.File({ filename: 'unexpected_errors.log', level: 'error' });
+
+const buildDefaultConsoleTransport = () => new transports.Console({ level: 'info' });
+
 export const logger = createLogger({
   level: 'debug',
   format: format.combine(
     format.timestamp(),
-    format.json(),
-    format.errors({ stack: true }), // Ref.: https://stackoverflow.com/a/58475687
-    format.prettyPrint(),
+    format.errors({ stack: true }), // Ref.: https://stackoverflow.com/a/5847568
+    format.simple(),
   ),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new transports.Console({ format: format.simple(), level: 'info' }),
+    buildDefaultConsoleTransport(),
     new transports.File({ filename: 'error.log', level: 'error' }),
     new transports.File({ filename: 'info.log', level: 'info' }),
     new transports.File({ filename: 'combined.log' }),
   ],
+  exceptionHandlers: [buildDefaultConsoleTransport(), buildUnexpectedErrorsFileTransport()],
+  rejectionHandlers: [buildDefaultConsoleTransport(), buildUnexpectedErrorsFileTransport()],
 });
