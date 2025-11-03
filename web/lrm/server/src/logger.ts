c4c15fe0bc14ca7deb7bcfe9f5c72cd813d9e21f
SimonClo <simon.cloitre@hotmail.fr>
2025-11-03 14:59:06 +0100

    chore(lrm): remove unused level variable in logger It is overridden by the level set in the console transport

--- web/lrm/server/src/logger.ts
+++ web/lrm/server/src/logger.ts
@@ -3,7 +3,6 @@ import { transports, createLogger, format } from 'winston';
 const buildDefaultConsoleTransport = () => new transports.Console({ level: 'info' });
 
 export const logger = createLogger({
-  level: 'debug',
   format: format.combine(
     format.timestamp(),
     format.errors({ stack: true }),
