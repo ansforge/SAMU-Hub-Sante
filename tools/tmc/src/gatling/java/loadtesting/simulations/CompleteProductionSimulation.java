a9d1fb91296e8ee87c70fa73902898f3cc2529d1
Clément Di Domenico <clement.didom@gmail.com>
2025-12-02 17:14:53 +0100

    feat(tmc/tools): rewrite production simulation with currently implemented scenarios

--- tools/tmc/src/gatling/java/loadtesting/simulations/CompleteProductionSimulation.java
+++ tools/tmc/src/gatling/java/loadtesting/simulations/CompleteProductionSimulation.java
@@ -1,15 +1,13 @@
 package loadtesting.simulations;
 
-import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.PopulationBuilder;
 import io.gatling.javaapi.core.Simulation;
 import loadtesting.AmqpConnectionFactory;
 import loadtesting.SimulationUtils;
-import org.galaxio.gatling.amqp.javaapi.protocol.AmqpProtocolBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static io.gatling.javaapi.core.CoreDsl.constantUsersPerSec;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import java.util.ArrayList;
 
 
 public class CompleteProductionSimulation extends Simulation {
@@ -19,43 +17,14 @@ public class CompleteProductionSimulation extends Simulation {
 
     {
         try {
-            String messageString = SimulationUtils.loadSampleMessage("rs-eda.json");
-            String invalidMessageString = SimulationUtils.loadSampleMessage("invalid.json");
-            String conversionMessageString = SimulationUtils.loadSampleMessage("conversion.json");
-            String traductionMessageString = SimulationUtils.loadSampleMessage("traduction.json");
-
             AmqpConnectionFactory connectionFactory = new AmqpConnectionFactory();
-            AmqpProtocolBuilder samuToSamuV1Connection = connectionFactory.buildAmqpProtocolBuilder("15-15_v1.5");
-            AmqpProtocolBuilder samuToSamuV3Connection = connectionFactory.buildAmqpProtocolBuilder("15-15_v2.1");
-            AmqpProtocolBuilder samuToNexsisV3Connection = connectionFactory.buildAmqpProtocolBuilder("15-nexsis_v1.9");
-            AmqpProtocolBuilder samuGpsConnection = connectionFactory.buildAmqpProtocolBuilder("15-gps_v1.3");
-
-            ScenarioBuilder standardScenario = scenario("RS-EDA").exec(SimulationUtils.sendAmqpMessage("fr.health.test.samuA", messageString));
-            ScenarioBuilder conversionScenario = scenario("Convert RS-EDA v1 to v3").exec(SimulationUtils.sendAmqpMessage("fr.health.test.samuv1", conversionMessageString));
-            ScenarioBuilder translationScenario = scenario("Translate RS-EDA to RC-EDA").exec(SimulationUtils.sendAmqpMessage("fr.health.test.samuv3", traductionMessageString));
-            ScenarioBuilder invalidMessageScenario = scenario("Invalid message").exec(SimulationUtils.sendAmqpMessage("fr.health.test.samuB", invalidMessageString));
-            ScenarioBuilder samuGpsScenario = scenario("GEO-POS").exec(SimulationUtils.sendAmqpMessage("fr.health.test.samuA", messageString));
-
             int duration = SimulationUtils.getNumericEnvVar("SCENARIO_DURATION", defaultDuration);
-            int samuGpsUserCount = SimulationUtils.getNumericEnvVar("SAMU_GPS_SCENARIO_USER_COUNT", defaultDuration);
 
-            setUp(
-                    conversionScenario.injectOpen(
-                            constantUsersPerSec(5).during(duration)
-                    ).protocols(samuToSamuV1Connection),
-                    standardScenario.injectOpen(
-                            constantUsersPerSec(10).during(duration)
-                    ).protocols(samuToSamuV1Connection),
-                    translationScenario.injectOpen(
-                            constantUsersPerSec(5).during(duration)
-                    ).protocols(samuToSamuV3Connection),
-                    invalidMessageScenario.injectOpen(
-                            constantUsersPerSec(5).during(duration)
-                    ).protocols(samuToNexsisV3Connection),
-                    samuGpsScenario.injectOpen(
-                            constantUsersPerSec(samuGpsUserCount).during(duration)
-                    ).protocols(samuGpsConnection)
-            ).maxDuration(duration * 2L);
+            ArrayList<PopulationBuilder> scenarios = new ArrayList<>();
+            scenarios.add(SamuGpsSimulation.setupScenarioPopulation(duration, connectionFactory));
+            scenarios.addAll(SamuNexsisSimulation.setupScenarioPopulation(duration, connectionFactory));
+
+            setUp(scenarios).maxDuration(duration * 2L);
         } catch (Exception e) {
             log.error("Unexpected error during load test", e);
         }
