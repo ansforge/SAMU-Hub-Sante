8f098db3feca453fcfeb8a410b16854ea96e02c9
SimonClo <simon.cloitre@hotmail.fr>
2025-12-23 09:26:46 +0100

    refactor(dispatcher): remove duplicated error log

--- hub/dispatcher/src/main/java/com/hubsante/hub/utils/MessageUtils.java
+++ hub/dispatcher/src/main/java/com/hubsante/hub/utils/MessageUtils.java
@@ -149,19 +149,7 @@ public class MessageUtils {
                             "The received message classname (%s) is not supported on this vhost",
                             messageClassName);
             String distributionId = edxlMessage.getDistributionID();
-            String senderId = edxlMessage.getSenderID();
             String recipientId = getRecipientID(edxlMessage);
-            structuredLog.error(
-                    errorMessage,
-                    Map.of(
-                            LogConstants.DISTRIBUTION_ID,
-                            distributionId,
-                            LogConstants.SENDER_ID,
-                            senderId,
-                            LogConstants.RECIPIENT_ID,
-                            recipientId,
-                            LogConstants.MESSAGE_TYPE,
-                            messageClassName));
             throw new UnroutableMessageException(
                     errorMessage, distributionId, recipientId, messageClassName);
         }
