69dc3c6fd21915aa14a462a9493d09a67999c6d3
Issam Kadar <issam@kadar.pro>
2026-03-06 17:42:52 +0100

    feature/dispatcher : add mongodb configuration to enable auditing

---
+++ hub/dispatcher/src/main/java/com/hubsante/hub/config/MongoConfiguration.java
@@ -0,0 +1,8 @@
+package com.hubsante.hub.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfiguration {}
