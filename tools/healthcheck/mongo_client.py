5ad2cb4846a4a566b76a799835d7dd9e21e1a0cd
bnx <benjamin.bonche@gmail.com>
2026-04-15 09:12:16 +0200

    feat (healthcheck): add uri failback

--- tools/healthcheck/mongo_client.py
+++ tools/healthcheck/mongo_client.py
@@ -1,6 +1,6 @@
 import os
 from pymongo import MongoClient
 
-MONGODB_URI = os.getenv("MONGODB_URI")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 
 mongo_client = MongoClient(MONGODB_URI, serverSelectionTimeoutMS=1000)
