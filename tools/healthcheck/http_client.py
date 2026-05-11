fc3ee1568fec9c369aa74a1b55d688d2e3c8e88c
Clément Di Domenico <clement.didom@gmail.com>
2026-05-11 17:27:40 +0200

    tmp(healthcheck): add retry mechanism to http_client

--- tools/healthcheck/http_client.py
+++ tools/healthcheck/http_client.py
@@ -1,9 +1,26 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 from config import HTTP_TIMEOUT
 
 
 class TimeoutSession(requests.Session):
+    def __init__(self) -> None:
+        super().__init__()
+        retry = Retry(
+            total=1,
+            connect=1,
+            read=0,
+            status=0,
+            allowed_methods={"GET"},
+            backoff_factor=0,
+            raise_on_status=False,
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        self.mount("http://", adapter)
+        self.mount("https://", adapter)
+
     def request(self, method, url, **kwargs):
         kwargs.setdefault("timeout", HTTP_TIMEOUT)
         return super().request(method, url, **kwargs)
