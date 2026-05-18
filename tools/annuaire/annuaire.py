f061ce7aca3c4fe1022cfd6ffa7bcc7dacb31786
Issam KADAR <issam@kadar.pro>
2026-05-18 17:48:17 +0200

    feat/annuaire: replace VALUES_DIR+VALUES_FILENAME with single VALUES_PATH env var

--- tools/annuaire/annuaire.py
+++ tools/annuaire/annuaire.py
@@ -14,11 +14,7 @@ VALUES_FILE_PATH = os.environ.get(
 API_ENDPOINT = "/annuaire/api"
 HEALTH_ENDPOINT = "/annuaire/health"
 
-VALUES_DEFAULT_DIR = "/config/topology"
-VALUES_FILENAME = "values.yaml"
-VALUES_PATH = os.path.join(
-    os.environ.get("VALUES_DIR", VALUES_DEFAULT_DIR), VALUES_FILENAME
-)
+VALUES_PATH = os.environ.get("VALUES_PATH", "/config/topology/values.yaml")
 CLIENTS_DATA_KEY = "CLIENTS_DATA"
 
 TOPOLOGY_ROOT_KEY = "annuaire"
