a93e0d0cae2e51084f0dde3c18ca16ddf23eed12
Issam KADAR <issam@kadar.pro>
2026-06-08 11:11:12 +0200

    refactor(annuaire): update VALUES_PATH default to /config/clients/values.yaml

--- tools/annuaire/annuaire.py
+++ tools/annuaire/annuaire.py
@@ -8,7 +8,7 @@ API_ENDPOINT = "/annuaire/api"
 CLIENTS_ENDPOINT = f"{API_ENDPOINT}/clients"
 HEALTH_ENDPOINT = "/annuaire/health"
 
-VALUES_PATH = os.environ.get("VALUES_PATH", "/config/topology/values.yaml")
+VALUES_PATH = os.environ.get("VALUES_PATH", "/config/clients/values.yaml")
 ANNUAIRE_CLIENTS_DATA_KEY = "ANNUAIRE_CLIENTS_DATA"
 
 ANNUAIRE_ROOT_KEY = "annuaire"
