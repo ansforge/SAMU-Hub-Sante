7c565a3fe006b72919deb8d3530e89b5358b71c8
elinoden <eliott.morcillo@theodo.com>
2026-07-16 15:03:49 +0200

    feat(specs/client): client bootstrap

---
+++ tools/specs/client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className="flex items-center px-6 py-4">
+      <img
+        src={`${import.meta.env.BASE_URL}logo-ANS-footer.svg`}
+        alt="ANS"
+        className="h-10"
+      />
+    </footer>
+  )
+}
