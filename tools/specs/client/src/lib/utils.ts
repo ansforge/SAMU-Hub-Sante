4ed081505f71a6146ce453d73683f6177c9b175a
elinoden <eliott.morcillo@theodo.com>
2026-08-11 09:22:17 +0200

    feat(specs/client): introduce global buildGithubRawUrl + refactor

--- tools/specs/client/src/lib/utils.ts
+++ tools/specs/client/src/lib/utils.ts
@@ -5,6 +5,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+function buildGithubRawUrl(ref: string, path: string): string {
+  return `https://raw.githubusercontent.com/ansforge/SAMU-Hub-Modeles/${ref}/${path}`;
+}
+
 export function buildGithubSchemaUrl(schemaName: string, ref: string): string {
-  return `https://raw.githubusercontent.com/ansforge/SAMU-Hub-Modeles/${ref}/src/main/resources/json-schema/${schemaName}`;
+  return buildGithubRawUrl(ref, `src/main/resources/json-schema/${schemaName}`);
+}
+
+export function buildNomenclatureUrl(nomenclature: string, ref: string): string {
+  return buildGithubRawUrl(
+    ref,
+    `nomenclature_parser/out/latest/json_schema/${nomenclature}.json`,
+  );
 }
