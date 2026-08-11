13f610207d227fefc4725fc563b2e744b34bab25
elinoden <eliott.morcillo@theodo.com>
2026-08-11 17:44:54 +0200

    feat(specs/client): update sidebar logo behavior

--- tools/specs/client/src/components/app-sidebar.tsx
+++ tools/specs/client/src/components/app-sidebar.tsx
@@ -10,13 +10,14 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar";
 
+import { defaultRef } from "@/config";
 import { Link } from "@tanstack/react-router";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <Link to="/">
+        <Link to="/" search={(prev) => ({ ref: prev.ref ?? defaultRef })}>
           <AssetImage name="logo-ANS-footer.svg" alt="ANS" size={40} />
         </Link>
       </SidebarHeader>
