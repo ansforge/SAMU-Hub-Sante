9fa65827699392da97e3979232ab92d4da1af538
Clément Di Domenico <clement.didom@gmail.com>
2026-08-14 14:43:10 +0200

    chore(specs/client): remove /specs prefix path on app

--- tools/specs/client/vite.config.ts
+++ tools/specs/client/vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import path from "node:path"
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/specs/" : "/",
+export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
@@ -15,4 +14,4 @@ export default defineConfig(({ command }) => ({
     host: true,
     allowedHosts: true,
   },
-}))
+})
