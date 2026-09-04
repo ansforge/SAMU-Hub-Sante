a02294a3ef23b7a37e59cfdb997707aaecb61424
elinoden <eliott.morcillo@theodo.com>
2026-09-04 12:11:21 +0200

    feat(specs): new shadcn toast component

--- tools/specs/client/src/main.tsx
+++ tools/specs/client/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router";
 import "./index.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./components/ui/toast";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <RouterProvider router={router} />
+        <Toaster />
       </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>,
