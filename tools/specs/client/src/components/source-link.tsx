2bf76aad1c9e222331967e597a4b4f15ba385ef8
elinoden <eliott.morcillo@theodo.com>
2026-09-21 16:50:52 +0200

    feat(specs): update SourceLink to remove link label

--- tools/specs/client/src/components/source-link.tsx
+++ tools/specs/client/src/components/source-link.tsx
@@ -4,12 +4,8 @@ interface SourceLink {
   href: string;
 }
 const SourceLink = ({ href }: SourceLink) => (
-  <a
-    target="_blank"
-    className="flex items-center text-sm underline text-primary"
-    href={href}
-  >
-    voir la source <ExternalLinkIcon className="h-3 w-3 ml-1" />
+  <a target="_blank" className="flex items-center text-primary" href={href}>
+    <ExternalLinkIcon className="h-4 w-4 ml-1" />
   </a>
 );
 
